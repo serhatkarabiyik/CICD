21b8f14ea30c36ead617c96904e161e0ee8bd3eb
Serhat KARABIYIK <95712348+serhatkarabiyik@users.noreply.github.com>
2024-09-26 11:21:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from inc_dec import increment, decrement
 
-print(f"increment(3) = {increment(3)}  # Devrait afficher 4")
-print(f"increment(0) = {increment(0)}  # Devrait afficher 1")
+def test_increment():
+  assert increment(3) == 4
 
-print(f"decrement(3) = {decrement(3)}  # Devrait afficher 2")
-print(f"decrement(0) = {decrement(0)}  # Devrait afficher -1")
+def test_decrement():
+  assert decrement(2) == 1
+  
